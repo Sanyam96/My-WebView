c331b8fadab1239dd71304150876674993bf6ed6
sanyam96 <you@example.com>
2016-08-14 21:53:41 +0530

    google in app

--- app/src/main/java/com/example/sanyam/mywebviewapp/MainActivity.java
+++ app/src/main/java/com/example/sanyam/mywebviewapp/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.sanyam.mywebviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        browser = (WebView) findViewById(R.id.webView);
+        browser.loadUrl("https://www.google.co.in");
     }
 }
