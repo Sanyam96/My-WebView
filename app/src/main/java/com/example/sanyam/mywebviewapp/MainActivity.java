fa74f413277ef3d57fa1bf5b67bc031b59dd6e72
sanyam96 <you@example.com>
2016-08-15 14:08:37 +0530

    added code url

--- app/src/main/java/com/example/sanyam/mywebviewapp/MainActivity.java
+++ app/src/main/java/com/example/sanyam/mywebviewapp/MainActivity.java
@@ -11,9 +11,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         browser = (WebView) findViewById(R.id.webView);
-        browser.loadUrl("https://www.google.co.in");
+        browser.loadUrl("https://ideone.com/EImE6H");
     }
 }
