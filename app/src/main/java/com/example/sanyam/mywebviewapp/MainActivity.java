600fff1ec645ef7c73bbd2e5536edeea758528a1
sanyam96 <you@example.com>
2016-08-15 16:22:35 +0530

    Enable JavaScript

--- app/src/main/java/com/example/sanyam/mywebviewapp/MainActivity.java
+++ app/src/main/java/com/example/sanyam/mywebviewapp/MainActivity.java
@@ -2,18 +2,24 @@ package com.example.sanyam.mywebviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView browser;
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        browser = (WebView) findViewById(R.id.webView);
-        browser.loadUrl("https://ideone.com/EImE6H");
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+        //browser.loadUrl("https://ideone.com/EImE6H");
+        mWebView.loadUrl("https://www.google.co.in");
     }
 }
